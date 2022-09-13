6d7369c4496a473fd07e65a5e826fde0ac5174ab
Cesar Estiven Mesa Medrano <cesar-2007-mesa@hotmail.com>
2022-09-12 22:27:51 -0500

    se agrega query methods basicos

---
+++ src/main/java/com/curso/SpringBoot/persistence/ProductoRepository.java
@@ -0,0 +1,25 @@
+package com.curso.SpringBoot.persistence;
+
+import com.curso.SpringBoot.persistence.crud.ProductoCrudRepository;
+import com.curso.SpringBoot.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoRepository {
+
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+}
--- src/main/java/com/curso/SpringBoot/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/curso/SpringBoot/persistence/crud/ProductoCrudRepository.java
@@ -1,4 +1,17 @@
 package com.curso.SpringBoot.persistence.crud;
 
-public interface ProductoCrudRepository implements {
+import com.curso.SpringBoot.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    //Existe dos formas, llamando nativamente o con los query methodsyp
+  //  @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
